84f07fd8ecd2045b70505af8d0063366eb280e14
ARJUN KUMAR MARUYA <arjunkumarmaruya@ARJUNs-MacBook-Air.local>
2025-02-12 08:36:04 +0530

    add animation in dropdown

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { IoIosSearch } from "react-icons/io";
 import { IoIosArrowDropdown } from "react-icons/io";
+import { motion, AnimatePresence } from "framer-motion";
 
 const BlogList = ( {blogList} ) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isTopicOpen, setIsTopicOpen] = useState(false);
+    const categoryOptions = ["All Categories", "Category 1", "Category 2", "Category 3"];
+    const topicOptions = ["All Topics", "Topic 1", "Topic 2", "Topic 3"];
+
+    const dropdownRef = useRef(null);
+    
+    // Close dropdown when clicking outside
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
   return (
     <>
         <div className='md:flex md:flex-wrap p-5 mt-5'>
@@ -19,17 +38,63 @@ const BlogList = ( {blogList} ) => {
                 />
                 <IoIosSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-orange-600 text-3xl" />
             </div>
-            <div className='relative md:pr-2 mt-4 w-full md:w-1/2 xl-w-auto xl:flex-1 xl:mt-0'>
-                <select className='p-3 border-b w-full max-w-full overflow-hidden mb-2 appearance-none outline-none rounded-none'>
-                    <option>Topic</option>
-                </select>
-                <IoIosArrowDropdown className='absolute right-3 top-1/2 transform -translate-y-1/2 text-2xl' />
+            <div ref={dropdownRef} className='relative md:pr-0 mt-4 w-full md:w-1/2 xl-w-auto xl:flex-1 xl:mt-0'>
+                <div className="p-3 border-b border-b-neutral-800 w-full cursor-pointer flex justify-between items-center" onClick={() => setIsTopicOpen(!isTopicOpen)}>
+                    <span>Topic</span>
+                    <IoIosArrowDropdown className={`text-2xl transition-transform ${isTopicOpen ? "rotate-180" : ""}`} />
+                </div>
+
+                <AnimatePresence>
+                    {isTopicOpen && (
+                        <motion.ul
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: isTopicOpen ? "auto" : 0, opacity: isTopicOpen ? 1 : 0 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.5, ease: "easeInOut" }}
+                        className="absolute left-0 w-full bg-white border border-gray-300 shadow-md z-10 overflow-hidden"
+                    >
+                            {topicOptions.map((option, index) => (
+                                <li
+                                    key={index}
+                                    className="p-2 hover:text-orange-600 cursor-pointer"
+                                    transition={{duration: 0.4}}
+                                    onClick={() => setIsTopicOpen(false)}
+                                >
+                                    {option}
+                                </li>
+                            ))}
+                        </motion.ul>
+                    )}
+                </AnimatePresence>
             </div>
-            <div className='relative md:pl-2 mt-4 w-full md:w-1/2 xl-w-auto xl:flex-1 xl:mt-0'>
-                <select className='p-3 border-b w-full mb-2 appearance-none outline-none rounded-none'>
-                    <option>All Categories</option>
-                </select>
-                <IoIosArrowDropdown className='absolute right-3 top-1/2 transform -translate-y-1/2 text-2xl' />
+            <div ref={dropdownRef} className="relative md:pl-2 mt-4 w-full md:w-1/2 xl:w-auto xl:flex-1 xl:mt-0">
+                <div className="p-3 border-b border-b-neutral-800 w-full cursor-pointer flex justify-between items-center" onClick={() => setIsOpen(!isOpen)}>
+                    <span>Select Category</span>
+                    <IoIosArrowDropdown className={`text-2xl transition-transform ${isOpen ? "rotate-180" : ""}`} />
+                </div>
+
+                <AnimatePresence>
+                    {isOpen && (
+                        <motion.ul
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: isOpen ? "auto" : 0, opacity: isOpen ? 1 : 0 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.5, ease: "easeInOut" }}
+                        className="absolute w-full bg-white border border-gray-300 shadow-md z-10 overflow-hidden"
+                    >
+                            {categoryOptions.map((option, index) => (
+                                <li
+                                    key={index}
+                                    className="p-2 hover:text-orange-600 cursor-pointer"
+                                    transition={{duration: 0.4}}
+                                    onClick={() => setIsOpen(false)}
+                                >
+                                    {option}
+                                </li>
+                            ))}
+                        </motion.ul>
+                    )}
+                </AnimatePresence>
             </div>
         </div>
         {blogList.map((blog, index) => (
